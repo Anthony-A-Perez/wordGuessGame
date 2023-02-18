55ce96908ba3c22be74a701234055e3b11716186
Anthony-A-Perez <ap.erez@me.com>
2023-02-18 13:39:22 -0800

    Added T/F statements

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+const statements = [
+  {
+    statement: 'You can use a text string as a literal value or assign it to a variable.',
+    answer: true 
+  },
+  {
+    statement: 'Assignment operators are used to perform mathematical calculations, such as addition, subtraction, multiplication, and division.',
+    answer: false 
+  },
+  {
+    statement: "The statements inside an 'if' statement are contained by the same curly braces used to contain the statements in a function.",
+    answer: true
+  },
+  {
+    statement: "JavaScript arrays are zero-indexed. The first element of an array is indexed at 0, and the last element is at the index value equal to the value of the array's length property.",
+    answer: true
+  },
+  {
+    statement: "Learning JavaScript is fun?",
+    answer: true 
+  },
+  {
+    statement: "String values must be enclosed within parenthesis when being assigned to variables.",
+    answer: false
+  },
+  {
+    statement: "The JavaScript programming language was first created by Brendan Eich.",
+    answer: true
+  },
+  {
+    statement: "Methods are a commonly used data type.",
+    answer: false 
+  },
+  {
+    statement: "JavaScript is the same as Java.",
+    answer: false 
+  },
+  {
+    statement: "We put JavaScript inside the <js> HTML element.",
+    answer: false 
+  }
+]

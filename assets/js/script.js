fa8312554811d9f4a6a63055ecb86d874d335bab
Anthony-A-Perez <ap.erez@me.com>
2023-02-21 18:23:54 -0800

    assigned values to T/F statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,38 +6,42 @@
 // statements to be evaluated, T or F
 
 const questions = [
-  
-    "You can use a text string as a literal value or assign it to a variable.",
-  
-    "Assignment operators are used to perform mathematical calculations, such as addition, subtraction, multiplication, and division.",
 
+    "You can use a text string as a literal value or assign it to a variable.",
+
+    "Assignment operators are used to perform mathematical calculations, such as addition, subtraction, multiplication, and division.",
+    
     "The statements inside an 'if' statement are contained by the same curly braces used to contain the statements in a function.",
 
     "JavaScript arrays are zero-indexed. The first element of an array is indexed at 0, and the last element is at the index value equal to the value of the array's length property.",
 
     "Learning JavaScript is fun?",
-
+    
     "String values must be enclosed within parenthesis when being assigned to variables.",
-
+    
     "The JavaScript programming language was first created by Brendan Eich.",
-  
+    
     "Methods are a commonly used data type.",
-  
+    
     "JavaScript is the same as Java.",
-  
+    
     "We put JavaScript inside the <js> HTML element.",
+    
 
 ];
 
-let trueStatements = [questions[0],questions[2],questions[3],questions[4],questions[6]];
-let falseStatements = [questions[1],questions[5],questions[7],questions[8],questions[9]];
-let statementValueTrue = trueStatements == "0";
-let statementValueFalse = falseStatements == "1";
-const winText = "Correct, be wary of success.";
-const loseText = "Incorrect, you're perfectly flawed.";
+// created value of "0" for true statements and value of "1" for false statements
+
+const trueStatements = [questions[0],questions[2],questions[3],questions[4],questions[6]];
+trueStatements.value = 0;
+
+const falseStatements = [questions[1],questions[5],questions[7],questions[8],questions[9]];
+falseStatements.value = 1;
+
+
 
 // get referenced elements
-var answer = "0"
+
 var trueButton = document.getElementById("true-button");
 var falseButton = document.getElementById("false-button");
 var answerButton = document.querySelector(".answer-button");
@@ -51,30 +55,37 @@ var startButton = document.querySelector(".start-button");
 // create function to get next statement
 
 function getStatement() {
-let rand = Math.random(); 
-let totalQuestions = questions.length;
-let randIndex = Math.floor(rand * totalQuestions);
-let randomQuestion = questions[randIndex]
-statement.innerText = randomQuestion;
-//result.style.display = "none";
-
+    let rand = Math.random(); 
+    let totalQuestions = trueStatements.length + falseStatements.length;
+    let randIndex = Math.floor(rand * totalQuestions);
+    let randomQuestion = questions[randIndex]
+    statement.innerText = randomQuestion;
+    
 };
 
-answerButton.addEventListener("click", event => {
-    if (event.target.value == answer) {
+//var hideResult = result.style.display = "none";
+
+// compares value of statement with vale of click event
+
+answerButton.addEventListener("click", event=> {
+    
+const winText = "Correct, be wary of success.";
+const loseText = "Incorrect, you're perfectly flawed.";
+
+    if (event.target.value == trueStatements.value) {
         result.innerText = winText;
     } else {
-        result.innerText =loseText;
+            result.innerText = loseText;
+        
+
     }
-});
+}  
 
-
-
-
-
-
+);
 
 startButton.addEventListener("click", getStatement);
+
+
 
 
 
